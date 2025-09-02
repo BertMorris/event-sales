d50df47e7018790df14f69f626b8296d61b0c0db
Robert Morrison <bertmorris911@gmail.com>
2025-09-02 16:42:13 -0700

    fix ms graph config

--- src/main/java/com/bertmorris/event_management/EventManagementApplication.java
+++ src/main/java/com/bertmorris/event_management/EventManagementApplication.java
@@ -2,8 +2,10 @@ package com.bertmorris.event_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class EventManagementApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bertmorris/event_management/email/provider/MsGraphConfig.java
+++ src/main/java/com/bertmorris/event_management/email/provider/MsGraphConfig.java
@@ -2,11 +2,18 @@ package com.bertmorris.event_management.email.provider;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+
 @ConfigurationProperties(prefix = "microsoft")
 public record MsGraphConfig(
+    @NotBlank
     String clientId,
+    @NotBlank
     String clientSecret,
+    @NotBlank
     String tenantId,
+    @NotEmpty
     String[] scopes
 ) {
 }
